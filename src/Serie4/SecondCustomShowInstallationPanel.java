6b9dd96951bebefbcdebccecb81253630ff627ad
PuffBallPoyo <46196874+PuffBallPoyo@users.noreply.github.com>
2020-05-19 16:13:58 +0200

    fully implemented second request

--- src/Serie4/SecondCustomShowInstallationPanel.java
+++ src/Serie4/SecondCustomShowInstallationPanel.java
@@ -20,11 +20,75 @@ import java.util.Date;
 import javax.swing.JFormattedTextField.AbstractFormatter;
 
 public class SecondCustomShowInstallationPanel extends JPanel {
+    private JLabel dummyLabel,editorTableLabel;
+    private JButton findButton;
+    private JTable editorTable,requestTable;
+    private JScrollPane SQLtable;
+
+    private validationActionManager findButtonListener = new validationActionManager();
 
     private Connection connection;
 
     public SecondCustomShowInstallationPanel(Connection connection) {
         this.connection = connection;
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        editorTableLabel = new JLabel("Sélectionnez un éditeur à chercher : ");
+        add(editorTableLabel);
 
+        try {
+            // Requests and shows SQL table
+            String sqlInstruction = "SELECT CodeEdit,Designation FROM Editeur;";
+            PreparedStatement prepStat = connection.prepareStatement(sqlInstruction);
+            TableModelGen GenericModel = AccessBDGen.creerTableModel(prepStat);
+            editorTable = new JTable(GenericModel);
+            JScrollPane SQLtable = new JScrollPane(editorTable);
+            SQLtable.setPreferredSize(new Dimension(450, 230));
+            hideColumn(editorTable);
+            add(SQLtable);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        findButton = new JButton("Rechercher");
+        findButton.addActionListener(findButtonListener);
+        add(findButton);
+
+        dummyLabel = new JLabel("empty");
+        dummyLabel.setVisible(false);
+        add(dummyLabel);
+    }
+    private class validationActionManager implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String editorInput = editorTable.getValueAt(editorTable.getSelectedRow(), 0).toString();
+            String instruction = "SELECT * FROM Software WHERE CodeEdit = '" + editorInput + "'";
+            SQLRequest(instruction, 900, 300);
+        }
+    }
+    public void SQLRequest(String instruction, int w, int h)
+    {
+        try {
+            // Requests and shows SQL table
+            remove(getComponent(3));
+            String sqlInstruction = instruction + ";";
+            PreparedStatement prepStat = connection.prepareStatement(sqlInstruction);
+            TableModelGen GenericModel = AccessBDGen.creerTableModel(prepStat);
+            requestTable = new JTable(GenericModel);
+            JScrollPane SQLtableX = new JScrollPane(requestTable);
+            SQLtableX.setPreferredSize(new Dimension(w, h));
+            add(SQLtableX);
+            revalidate();
+            repaint();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void hideColumn(JTable table){
+        table.getColumnModel().getColumn(0).setMinWidth(0);
+        table.getColumnModel().getColumn(0).setMaxWidth(0);
+        table.getColumnModel().getColumn(0).setResizable(false);
     }
 }
